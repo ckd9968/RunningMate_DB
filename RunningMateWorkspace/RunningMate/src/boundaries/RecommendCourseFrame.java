54c1909ea2990dca5345fcfeb93ac8e94b8d821f
이해창 <gockd9968@naver.com>
2023-12-07 04:28:21 +0900

    추천코스검색UI수정, 추천코스검색컨트롤러
    추천코스검색UI수정, 추천코스검색

--- RunningMateWorkspace/RunningMate/src/boundaries/RecommendCourseFrame.java
+++ RunningMateWorkspace/RunningMate/src/boundaries/RecommendCourseFrame.java
@@ -1,20 +1,34 @@
 package boundaries;
 
+import controllers.RecommendCourseController;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTable;
 import javax.swing.JButton;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class RecommendCourseFrame extends JFrame {
 
 	private JPanel contentPane;
-	private JTable table;
+	private JScrollPane scrollPane;
+	private JTable recommendCourseTable;
+	private RecommendCourseController recommendCourseController;
+	private String[] columnNames = {"주소", "분위기", "방문수"};
 
 	/**
 	 * Launch the application.
@@ -32,60 +46,162 @@ public class RecommendCourseFrame extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 * callable procedure. prepared, statement
-	 */
 	public RecommendCourseFrame() {
+		recommendCourseController = new RecommendCourseController();
+		
+		DefaultTableModel model = new DefaultTableModel(
+	            new Object[][] {},
+	            columnNames
+	        );
+		
 		setAlwaysOnTop(true);
 		
 		setVisible(true);
 		
 		setTitle("추천 코스");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 413, 300);
+		setBounds(100, 100, 609, 372);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(10, 21, 55, 23);
-		contentPane.add(comboBox);
-		
-		JLabel lblNewLabel = new JLabel("도");
-		lblNewLabel.setBounds(75, 25, 18, 15);
-		contentPane.add(lblNewLabel);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(103, 21, 55, 23);
-		contentPane.add(comboBox_1);
-		
-		JLabel lblNewLabel_1 = new JLabel("시");
-		lblNewLabel_1.setBounds(168, 25, 18, 15);
-		contentPane.add(lblNewLabel_1);
+		JComboBox addressBox = new JComboBox();
+		addressBox.setModel(new DefaultComboBoxModel(new String[] {"전체", "서울", "인천", "대전", "대구", "부산", "경주", "성남", "고양"}));
+		addressBox.setBounds(133, 21, 73, 23);
+		contentPane.add(addressBox);
 		
 		JLabel lblNewLabel_2 = new JLabel("분위기");
-		lblNewLabel_2.setBounds(196, 25, 50, 15);
+		lblNewLabel_2.setBounds(61, 58, 50, 15);
 		contentPane.add(lblNewLabel_2);
 		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"전체", "좋음", "보통", "나쁨"}));
-		comboBox_2.setBounds(242, 21, 50, 23);
-		contentPane.add(comboBox_2);
+		JComboBox atmosphereBox = new JComboBox();
+		atmosphereBox.setModel(new DefaultComboBoxModel(new String[] {"전체", "좋음", "보통", "나쁨"}));
+		atmosphereBox.setBounds(133, 54, 73, 23);
+		contentPane.add(atmosphereBox);
 		
-		table = new JTable();
-		table.setBounds(10, 71, 359, 119);
-		contentPane.add(table);
-		
-		JButton btnNewButton = new JButton("검색하기");
-		btnNewButton.setBounds(302, 21, 93, 23);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("닫기");
-		btnNewButton_1.setBounds(278, 232, 93, 23);
-		contentPane.add(btnNewButton_1);
-	}
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(61, 139, 464, 149);
+		contentPane.add(scrollPane);
 
+		recommendCourseTable = new JTable();
+		recommendCourseTable.setModel(model);
+		scrollPane.setViewportView(recommendCourseTable);
+		
+		JButton closeButton = new JButton("닫기");
+		closeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		closeButton.setBounds(230, 304, 93, 23);
+		contentPane.add(closeButton);
+		
+		JLabel lblNewLabel = new JLabel("지역");
+		lblNewLabel.setBounds(61, 25, 50, 15);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("방문수");
+		lblNewLabel_1.setBounds(61, 96, 50, 15);
+		contentPane.add(lblNewLabel_1);
+		
+		JTextField visitCountOver = new JTextField();
+		visitCountOver.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				int maxLength = 4;
+				char c = e.getKeyChar();
+				
+		        if (!Character.isDigit(c)) {
+		            e.consume(); // 입력된 값이 숫자가 아니면 입력을 무시
+		        }else if (visitCountOver.getText().length() >= maxLength) {
+		            e.consume(); // 최대 길이 초과 시 입력을 무시
+		        }
+			}
+		});
+		visitCountOver.setBounds(133, 93, 73, 21);
+		contentPane.add(visitCountOver);
+		visitCountOver.setColumns(10);
+		
+		
+		JLabel lblNewLabel_3 = new JLabel("이상");
+		lblNewLabel_3.setBounds(216, 96, 50, 15);
+		contentPane.add(lblNewLabel_3);
+		
+		JTextField visitCountUnder = new JTextField();
+		visitCountUnder.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				int maxLength = 4;
+				char c = e.getKeyChar();
+				
+		        if (!Character.isDigit(c)) {
+		            e.consume();
+		        }else if (visitCountUnder.getText().length() >= maxLength) {
+		            e.consume();
+		        }
+			}
+		});
+		visitCountUnder.setBounds(253, 93, 73, 21);
+		contentPane.add(visitCountUnder);
+		visitCountUnder.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("이하");
+		lblNewLabel_4.setBounds(336, 96, 50, 15);
+		contentPane.add(lblNewLabel_4);
+		
+		JButton searchButton = new JButton("검색하기");
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String selectedAddress = addressBox.getSelectedItem().toString();
+				String selectedAtmosphere = atmosphereBox.getSelectedItem().toString();
+			    // 방문수를 입력하지 않았을 경우 검색값을 0과 9999로 하기 위해 선언
+				int visitOver = 0;
+			    int visitUnder = 9999;
+			    
+			    String visitOverText = visitCountOver.getText();
+			    String visitUnderText = visitCountUnder.getText();
+			    
+			    if (visitOverText != null && !visitOverText.isEmpty()) {
+			        visitOver = Integer.parseInt(visitOverText);
+			    }
+			    
+			    if (visitUnderText != null && !visitUnderText.isEmpty()) {
+			        visitUnder = Integer.parseInt(visitUnderText);
+			    }	
+				
+				recommendCourseController.searchCourse(selectedAddress, selectedAtmosphere, visitOver, visitUnder);
+				
+				ResultSet rs = recommendCourseController.searchCourse(selectedAddress, selectedAtmosphere, visitOver, visitUnder);
+                DefaultTableModel model = (DefaultTableModel) recommendCourseTable.getModel();
+                model.setRowCount(0);
+
+                if (rs != null) {
+                    try {
+                        while (rs.next()) {
+                            String addressOut = rs.getString("주소");
+                            String atmosphereOut = rs.getString("분위기");
+                            int visitCount = rs.getInt("방문수");
+
+                            model.addRow(new Object[]{addressOut, atmosphereOut, visitCount});
+                        }
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    } finally {
+                        try {
+                            rs.close();
+                        } catch (SQLException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+		searchButton.setBounds(396, 20, 129, 90);
+		contentPane.add(searchButton);
+		
+
+
+	}
 }
---
+++ RunningMateWorkspace/RunningMate/src/controllers/RecommendCourseController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import oracle.jdbc.OracleTypes;
+
+
+public class RecommendCourseController {
+	private Connection con = null;
+	
+	public ResultSet  searchCourse(String selectedAddress, String selectedAtmosphere, int visitOver, int visitUnder) {
+		DB_Connector.connectToDB("코스추천 조회하기");
+		con = DB_Connector.getConnection();
+		ResultSet rs = null;
+		
+        try {
+            CallableStatement cstmt = con.prepareCall("{call SP_추천코스검색(?,?,?,?,?)}");
+            cstmt.setString(1, selectedAddress);
+            cstmt.setString(2, selectedAtmosphere);
+            cstmt.setInt(3, visitOver);
+            cstmt.setInt(4, visitUnder);
+            cstmt.registerOutParameter(5, OracleTypes.CURSOR);
+            cstmt.execute();
+            
+            rs = (ResultSet) cstmt.getObject(5);
+            
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+        return rs;
+	
+}
+
+}
